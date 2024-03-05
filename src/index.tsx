77969d7dda365c73b81455a71017fc6114fd195e
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2024-03-05 17:51:05 -0500

    fixed a small issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
+import "./css/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./css/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import { GameArea } from "./lib/GameArea";
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
     <React.StrictMode>
